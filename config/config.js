cce472579b1ea88c062214a3ea358c979be40505
Imran Ali Zaidi <imranalizaidi@yahoo.com>
2018-09-23 21:25:17 -0500

    OUTPUT_ASSETS flag added to dev script

--- config/config.js
+++ config/config.js
@@ -24,7 +24,7 @@ function outputAssets() {
   console.log('\n'); // eslint-disable-line no-console
 }
 
-if (process.env.NODE_ENV === 'development') { outputAssets(); }
+if (process.env.NODE_ENV === 'development' && process.env.OUTPUT_ASSETS) { outputAssets(); }
 
 config.paths = {
   models: modelPaths,
